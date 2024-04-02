c0250d1b6ad13371d7a4bc9eb134a72f58c017b2
R0drg0 <rodrigo.paiva@estudante.ifms.edu.br>
2024-04-02 09:03:38 -0300

    aula 1

---
+++ projeto1/App.js
@@ -0,0 +1,54 @@
+/*import React from "react";
+import {View, Text} from "react-native";
+
+function App(){
+    return(
+        <View>
+            <Text>Olá Mundo!</Text>
+        </View>
+    );
+}
+
+export default App;*/
+
+import React, {Component} from'react';
+import {View, Text, Image} from'react-native';
+
+class App extends Component{
+    render(){
+        let curso = 'INFO';
+
+        return(
+            <View>
+                <Text>Olá Mundo!</Text>
+                <Text>Exemplo!</Text>
+                <Text style={{color: 'blue', fontSize: 30, margin: 15}}></Text>
+
+                <Image source={{uri: 'coloca a url aqui'}} style = {{width: 300, height: 300}}/>
+                <Text>{curso}</Text>
+                <MinhaImagem largura={400} altura={400} nome="imagem 1"/>
+            </View>
+
+        );
+    
+    }
+}
+
+export default App;
+ 
+ class MinhaImagem  extends Component{
+   
+     render(){
+
+         let img ='url da imagem';
+         
+         return(
+             <View>
+                 <Image source={{url: img}} style={{width: this.props.largura, height: this.props.altura}}/>
+                 
+                 <Text> {this.props.nome}</Text>
+             </View>
+        )
+     }    
+ }
+
